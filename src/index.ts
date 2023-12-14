de6d507071a2f3ba3449a50c0d30431d402bf9ad
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-12-14 10:05:12 -0700

    refactor utils (#2)
    * refactor: add generatePdf function to generate pdf
    
    * refactor: add generatePdf function
    
    * style: the files have been formatted

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,20 @@
 import express, { Express } from "express";
-import puppeteer from "puppeteer";
+import { generatePdf } from "./utils/generatePdf";
 
 const PORT = 3000 || process.env.PORT;
 const app: Express = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.post("/generate", async (req, res) => {
-	const {url}: {url: string} = req.body;
-	const browser = await puppeteer.launch({headless: "new"});
-	const page = await browser.newPage();
-	await page.goto(url);
-	const pdf: Buffer = await page.pdf({ format: "a4", printBackground: true });
-	await browser.close();
+	const { url }: { url: string } = req.body;
+	const pdf = await generatePdf(url);
 	res.set({
 		"Content-Type": "application/pdf",
-	})
+	});
 	res.send(pdf);
-})
+});
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
