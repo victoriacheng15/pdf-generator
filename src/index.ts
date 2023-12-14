c996964c0131dd50a1ce970a9ec9f53fce006111
Victoria Cheng <victoria.cheng15@Gmail.com>
2023-12-14 10:04:33 -0700

    style: the files have been formatted

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express, { Express } from "express";
-import {generatePdf} from "./utils/generatePdf"
+import { generatePdf } from "./utils/generatePdf";
 
 const PORT = 3000 || process.env.PORT;
 const app: Express = express();
@@ -8,13 +8,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.post("/generate", async (req, res) => {
-	const {url}: {url: string} = req.body;
-	const pdf = await generatePdf(url)
+	const { url }: { url: string } = req.body;
+	const pdf = await generatePdf(url);
 	res.set({
 		"Content-Type": "application/pdf",
-	})
+	});
 	res.send(pdf);
-})
+});
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
--- src/utils/generatePdf.ts
+++ src/utils/generatePdf.ts
@@ -1,10 +1,10 @@
 import puppeteer from "puppeteer";
 
 export async function generatePdf(url: string) {
-  const browser = await puppeteer.launch({headless: "new"});
-  const page = await browser.newPage();
-  await page.goto(url);
-  const pdf: Buffer = await page.pdf({ format: "a4", printBackground: true });
-  await browser.close();
-  return pdf;
+	const browser = await puppeteer.launch({ headless: "new" });
+	const page = await browser.newPage();
+	await page.goto(url);
+	const pdf: Buffer = await page.pdf({ format: "a4", printBackground: true });
+	await browser.close();
+	return pdf;
 }
